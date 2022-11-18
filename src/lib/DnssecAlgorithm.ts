4bd3ff8d22cc06f3b0f760f37ebb4a3edcec8a46
Gus Narea <gnarea@users.noreply.github.com>
2022-11-18 12:39:31 +0000

    fix: Drop incomplete support for DSA/SHA1 (#55)
    See #50.

--- src/lib/DnssecAlgorithm.ts
+++ src/lib/DnssecAlgorithm.ts
@@ -4,7 +4,7 @@
  * @link https://www.iana.org/assignments/dns-sec-alg-numbers/dns-sec-alg-numbers.xhtml
  */
 export enum DnssecAlgorithm {
-  DSA = 3,
+  // DSA = 3, // See: https://github.com/relaycorp/dnssec-js/issues/50
   RSASHA1 = 5,
   RSASHA256 = 8,
   RSASHA512 = 10,
