cfc7a415a3bc1028b01f549e78f52d5dc6103206
Gus Narea <gnarea@users.noreply.github.com>
2022-11-19 11:54:43 +0000

    add missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+// DNS-related
+export { DnsClass } from './lib/dns/DnsClass';
+export { Header } from './lib/dns/Header';
 export { Message } from './lib/dns/Message';
 export { Question } from './lib/dns/Question';
+export { Record } from './lib/dns/Record';
+export { RRSet } from './lib/dns/RRSet';
+
+// DNSSEC-related
 export { DatePeriod } from './lib/verification/DatePeriod';
 export { Resolver } from './lib/verification/Resolver';
 export { UnverifiedChain } from './lib/verification/UnverifiedChain';
