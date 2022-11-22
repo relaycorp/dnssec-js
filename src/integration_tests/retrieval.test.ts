eb0e667026934e256c0fbf60ab90d9bd69f23d88
Gus Narea <gnarea@users.noreply.github.com>
2022-11-22 16:36:55 +0000

    chore: Write functional tests querying and validating real DNSSEC chains
    Fixes #53.

---
+++ src/integration_tests/retrieval.test.ts
@@ -0,0 +1,49 @@
+import { DNSoverHTTPS } from 'dohdec';
+
+import { Resolver } from '../lib/verification/Resolver';
+import { Message } from '../lib/dns/Message';
+import { UnverifiedChain, VerifiedChainResult } from '../lib/verification/UnverifiedChain';
+import { Question } from '../lib/dns/Question';
+import { SecurityStatus } from '../lib/verification/SecurityStatus';
+import { RRSet } from '../lib/dns/RRSet';
+import { DnsClass } from '../lib/dns/ianaClasses';
+import { FailureResult } from '../lib/verification/results';
+
+const DOH_CLIENT = new DNSoverHTTPS({ url: 'https://cloudflare-dns.com/dns-query' });
+afterAll(() => {
+  DOH_CLIENT.close();
+});
+
+const RESOLVER: Resolver = async (question) => {
+  const messageRaw = await DOH_CLIENT.lookup(question.name, {
+    rrtype: question.getTypeName(),
+    json: false,
+    decode: false,
+    dnssec: true, // Retrieve RRSig records
+  });
+  return Message.deserialise(messageRaw as Buffer);
+};
+
+test('Positive response in valid DNSSEC zone should be SECURE', async () => {
+  const question = new Question('dnssec-deployment.org.', 'A', DnsClass.IN);
+  const chain = await UnverifiedChain.retrieve(question, RESOLVER);
+
+  const result = chain.verify();
+
+  expect(result).toEqual<VerifiedChainResult>({
+    status: SecurityStatus.SECURE,
+    result: expect.any(RRSet),
+  });
+});
+
+test('Response from insecure zone should be INSECURE', async () => {
+  const question = new Question('dnssec-failed.org.', 'A', DnsClass.IN);
+  const chain = await UnverifiedChain.retrieve(question, RESOLVER);
+
+  const result = chain.verify();
+
+  expect(result).toEqual<FailureResult>({
+    status: SecurityStatus.INSECURE,
+    reasonChain: expect.arrayContaining([`Failed to verify zone ${question.name}`]),
+  });
+});
