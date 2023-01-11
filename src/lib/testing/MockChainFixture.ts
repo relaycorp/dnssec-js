d273797bc650b1892b82c08156fae1a6c8cc5056
Gus Narea <gnarea@users.noreply.github.com>
2023-01-11 19:17:04 +0000

    feat(MockChain): Output generated responses

--- src/lib/testing/MockChainFixture.ts
+++ src/lib/testing/MockChainFixture.ts
@@ -1,7 +1,9 @@
 import { type Resolver } from '../Resolver.js';
 import { type TrustAnchor } from '../TrustAnchor.js';
+import { type Message } from '../dns/Message.js';
 
 export interface MockChainFixture {
   readonly resolver: Resolver;
+  readonly responses: readonly Message[];
   readonly trustAnchors: readonly TrustAnchor[];
 }
