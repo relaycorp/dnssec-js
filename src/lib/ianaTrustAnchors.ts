7df03ff972307d0c2cfb80ee76a4d55e7f7ec63a
Veegish Ramdani <veegish240@gmail.com>
2024-09-06 17:38:23 +0400

    feat: Add new default IANA TA (#276)
    # What kind of change does this PR introduce?
    The new IANA TA which is expected to start working in 2026
    https://data.iana.org/root-anchors/root-anchors.xml
    
    # What is the current behavior? 
    N/A
    
    # What is the new behavior (if this is a feature change)?
    N/A

--- src/lib/ianaTrustAnchors.ts
+++ src/lib/ianaTrustAnchors.ts
@@ -15,4 +15,11 @@ export const IANA_TRUST_ANCHORS: readonly DsData[] = [
     DigestType.SHA256,
     Buffer.from('E06D44B80B8F1D39A95C0B0D7C65D08458E880409BBC683457104237C7F8EC8D', 'hex'),
   ),
+  new DsData(
+    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+    38_696,
+    DnssecAlgorithm.RSASHA256,
+    DigestType.SHA256,
+    Buffer.from('683D2D0ACB8C9B712A1948B27F741219298D0A450D612C483AF444A4C0FB2B16', 'hex'),
+  ),
 ];
